728a393f08b113a9134cb27a58505403a58dd831
Sumedh Vyas <sumedhvyas10@gmail.com>
2025-03-03 03:39:50 +0530

    {feature} - added env and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:firesync/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
+void main()async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
 
-void main() {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
